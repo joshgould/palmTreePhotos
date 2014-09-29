b5b874b33987004694fd47e03ac871bae23c6f9a
Josh Gould <joshgould@mbp628.local>
2014-09-29 00:43:11 -0700

    Retrieving sub page

--- cloud/app.js
+++ cloud/app.js
@@ -3,6 +3,40 @@ var express = require('express');
 var app = express();
 var $textBody = "";
 var $photoUrls = null;
+var $res = null;
+
+
+
+Parse.Cloud.define('httpRequest', function(request, response) {
+	Parse.Cloud.httpRequest({
+    url: request.params.url,
+	success: function(httpResponse) {
+		console.log("@@request url: " + request.params.url);
+		response.success(httpResponse);
+	    console.log(httpResponse.text);
+	  },
+	  error: function(httpResponse) {
+			response.error("httpRequest failed");    
+			console.error('Request failed with response code ' + httpResponse.status);
+	  }
+	});
+});
+
+
+
+function getPhoto(photoUrl) {
+	console.log("*** in getPhoto. URL: " + photoUrl);
+	
+	Parse.Cloud.run('httpRequest', {url:photoUrl}, {
+		success: function(res) {
+		 	console.log("getphoto res is"  + res);
+		},
+		error: function(error) {
+			console.log("getPhoto error " + error);
+		}
+	});	
+}
+
 
 Parse.initialize("KRoz8apqdtFgWLkOib5EbxOfvPPKYKaqIKzKQMrZ",
              "3JwT0X10HBPR525oMMGKzoUcF3VecebpFoiSyGyw");
@@ -37,7 +71,12 @@ function saveInfo() {
 	 			console.log("saveInfo error not saved" + photoUrls + error);
 			}
 		});
+	if ($photoUrls) {
+		for(var i=0; i<$photoUrls.length; i++) {
+			getPhoto($photoUrls[i]);
+		}
 	}
+}
 
 
 // Attach the Express app to Cloud Code.
