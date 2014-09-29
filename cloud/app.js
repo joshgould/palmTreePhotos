5682d4231732fcb8ae74826bd1dd4255dea9453d
Josh Gould <joshgould@mbp628.local>
2014-09-28 22:07:41 -0700

    Correctly parsing and storing photo urls

--- cloud/app.js
+++ cloud/app.js
@@ -2,39 +2,39 @@
 var express = require('express');
 var app = express();
 var $textBody = "";
+var $photoUrls = null;
 
 Parse.initialize("KRoz8apqdtFgWLkOib5EbxOfvPPKYKaqIKzKQMrZ",
              "3JwT0X10HBPR525oMMGKzoUcF3VecebpFoiSyGyw");
 
 app.use(express.bodyParser());    // Middleware for reading request body
 app.post('/test', function(req, res) {
-   res.send(req.body.TextBody);
-   $textBody = req.body.TextBody;
-   console.log('TextBody: ' + req.body.TextBody);	
-   var geturl = new RegExp(
-	  	"<((http|https)://www.kinderlime.com/photos/(([A-Za-z0-9$_.+!*(),;/?:@&~=-])|%[A-Fa-f0-9]{2}){2,}(#([a-zA-Z0-9][a-zA-Z0-9$_.+!*(),;/?:@&~=%-]*))?([A-Za-z0-9$_+!*();/?:~-])>)"
-		,"g"
-       );
-	var length = $textBody.match(geturl).length;
-	console.log('length: ' + length);	
-
-	var urls = $textBody.match(geturl); 
-	console.log('urls: ' + urls);	
-   saveInfo();
+   	res.send(req.body.TextBody);
+   	$textBody = req.body.TextBody;
+   	console.log('TextBody: ' + req.body.TextBody);	
+   	var geturl = new RegExp("((http|https)://www.kinderlime.com/photos/(([A-Za-z0-9$_.+!*(),;/?:@&~=-])|%[A-Fa-f0-9]{2}){2,}(#([a-zA-Z0-9][a-zA-Z0-9$_.+!*(),;/?:@&~=%-]*))?([A-Za-z0-9$_+!*();/?:~-]))","g");	
+	if ($textBody.match(geturl)) {
+		var length = $textBody.match(geturl).length;
+		console.log('length: ' + length);	
+		$photoUrls = $textBody.match(geturl); 
+		console.log('urls: ' + $photoUrls);	
+   		saveInfo();
+	}	
 });
 	
 
 function saveInfo() {
-	var Body = Parse.Object.extend("Body");
-	var body = new Body();	
-	body.save({
-	  body: $textBody
+	
+	var PhotoUrls = Parse.Object.extend("PhotoUrls");
+	var photoUrls = new PhotoUrls();	
+	photoUrls.save({
+	  photoUrls: $photoUrls
 	}, {
-	  		success: function(body) {
-	    		console.log("savInfo saved:" + body);
+	  		success: function(photoUrls) {
+	    		console.log("savInfo saved:" + photoUrls);
 			},
-	  		error: function(body, error) {
-	 			console.log("saveInfo error not saved" + body + error);
+	  		error: function(photoUrls, error) {
+	 			console.log("saveInfo error not saved" + photoUrls + error);
 			}
 		});
 	}
