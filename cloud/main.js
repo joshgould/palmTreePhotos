30069352199c58541fad9cf08b3ef0ca3ece48d9
Josh Gould <joshgould@mbp628.local>
2014-09-29 23:51:28 -0700

    in progress commit

--- cloud/main.js
+++ cloud/main.js
@@ -1,57 +1,19 @@
 require('cloud/app.js');
 
-Parse.Cloud.define("averageStars", function(request, response) {
-  	var query = new Parse.Query("Review");
-  	query.equalTo("movie", request.params.movie);
-	query.find({
-    success: function(results) {
-      var sum = 0;
-      for (var i = 0; i < results.length; ++i) {
-        sum += results[i].get("stars");
-      }
-      response.success(sum / results.length);
-    },
-    error: function() {
-      response.error("movie lookup failed");
-    }
-  });
-});
-
-
-Parse.Cloud.define("getRatings", function(request, response) {
-  	var query = new Parse.Query("Review");
-	query.select("movie","stars");
+Parse.Cloud.define("getAllPhotos", function(request, response) {
+  	var query = new Parse.Query("PhotoUrl");
+	query.select("photoUrl");
 	query.find({
     success: function(results) {
 		response.success(results);
     },
     error: function() {
-      response.error("movie lookup failed");
+      response.error("photo lookup failed");
     }
   });
 });
 
 
-Parse.Cloud.define("getUniques", function(request, response) {
-  	var query = new Parse.Query("Review");
-	query.select("movie");
-	query.find({
-    success: function(results) {
-		var uniques = [];
- 		for (var i= 0; i<results.length; ++i) {
-			var movie = results[i].get("movie");
-			if (uniques.indexOf(movie) < 0) { 
-				uniques.push(movie);
-			}
-		}  
-	//	console.log(uniques);
-		response.success(uniques);
-    },
-    error: function() {
-      response.error("movie lookup failed");
-    }
-  });
-});
 
 
 
